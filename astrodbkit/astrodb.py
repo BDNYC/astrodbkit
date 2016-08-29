eb4bd0de702b679336340c6c5b55d7e9c4f9e1fe
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-08-29 15:12:49 -0400

    minor fixes

--- astrodbkit/astrodb.py
+++ astrodbkit/astrodb.py
@@ -1230,13 +1230,20 @@ class Database:
                 create_txt = "CREATE TABLE {0} ({1}".format(table, ', '.join(
                         ['{} {} {}'.format(c, t, r) for c, t, r in zip(columns, types, constraints)]))
                 create_txt += ', PRIMARY KEY({}))'.format(', '.join([elem for elem in pk]))
-                print(create_txt.replace(',', ',\n'))
+                # print(create_txt.replace(',', ',\n'))
                 self.list(create_txt)
 
                 # Populate the new table and drop the old one
                 old_columns = [c for c in self.query("PRAGMA table_info(TempOldTable)", unpack=True)[1] if c in columns]
                 self.list("INSERT INTO {0} ({1}) SELECT {1} FROM TempOldTable".format(table, ','.join(old_columns)))
-                self.list("DROP TABLE TempOldTable")
+
+                # Check for and add any foreign key constraints
+                t = self.query('PRAGMA foreign_key_list(TempOldTable)', fmt='table')
+                if not isinstance(t, type(None)):
+                    self.list("DROP TABLE TempOldTable")
+                    self.add_foreign_key(table, t['table'].tolist(), t['from'].tolist(), t['to'].tolist())
+                else:
+                    self.list("DROP TABLE TempOldTable")
 
             # If the table does not exist and new_table is True, create it
             elif table not in tables and new_table:
@@ -1298,8 +1305,8 @@ class Database:
 
         try:
             # Rename the old table and create a new one
-            self.list("DROP TABLE IF EXISTS TempOldTable")
-            self.list("ALTER TABLE {0} RENAME TO TempOldTable".format(table))
+            self.list("DROP TABLE IF EXISTS TempOldTable_foreign")
+            self.list("ALTER TABLE {0} RENAME TO TempOldTable_foreign".format(table))
 
             # Re-create the table specifying the FOREIGN KEY
             sqltxt = "CREATE TABLE {0} ({1}".format(table, ', '.join(['{} {} {}'.format(c, t, r)
@@ -1307,17 +1314,17 @@ class Database:
             sqltxt += ', PRIMARY KEY({})'.format(', '.join([elem for elem in pk_names]))
             if isinstance(key_child, type(list())):
                 for kc, p, kp in zip(key_child, parent, key_parent):
-                    sqltxt += ', FOREIGN KEY ({0}) REFERENCES {1} ({2})'.format(kc, p, kp)
+                    sqltxt += ', FOREIGN KEY ({0}) REFERENCES {1} ({2}) ON UPDATE CASCADE'.format(kc, p, kp)
             else:
-                sqltxt += ', FOREIGN KEY ({0}) REFERENCES {1} ({2})'.format(key_child, parent, key_parent)
+                sqltxt += ', FOREIGN KEY ({0}) REFERENCES {1} ({2}) ON UPDATE CASCADE'.format(key_child, parent, key_parent)
             sqltxt += ' )'
 
             self.list(sqltxt)
 
             # Populate the new table and drop the old one
-            old_columns = [c for c in self.query("PRAGMA table_info(TempOldTable)", unpack=True)[1] if c in columns]
-            self.modify("INSERT INTO {0} ({1}) SELECT {1} FROM TempOldTable".format(table, ','.join(old_columns)))
-            self.list("DROP TABLE TempOldTable")
+            old_columns = [c for c in self.query("PRAGMA table_info(TempOldTable_foreign)", unpack=True)[1] if c in columns]
+            self.list("INSERT INTO {0} ({1}) SELECT {1} FROM TempOldTable_foreign".format(table, ','.join(old_columns)))
+            self.list("DROP TABLE TempOldTable_foreign")
 
             if verbose:
                 print('Successfully added foreign key.')
@@ -1327,7 +1334,7 @@ class Database:
         except:
             print('Error attempting to add foreign key.')
             self.list("DROP TABLE IF EXISTS {0}".format(table))
-            self.list("ALTER TABLE TempOldTable RENAME TO {0}".format(table))
+            self.list("ALTER TABLE TempOldTable_foreign RENAME TO {0}".format(table))
 
         # Reactivate foreign keys
         self.list('PRAGMA foreign_keys=ON')
