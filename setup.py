77c9bf1707fd35d7cf48765e9a8f725966c9f49f
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-04-03 14:40:29 -0400

    Added sklearn to the setup.py file.

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     ],
     keywords='astrophysics',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['numpy','astropy','matplotlib','requests','pandas'],
+    install_requires=['numpy','astropy','matplotlib','requests','pandas','sklearn'],
 
 )
